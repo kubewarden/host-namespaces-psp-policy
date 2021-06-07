835468feabe1dd078dc23a90ccf0b27b3eb57633
Rafael Fernández López <ereslibre@ereslibre.es>
2021-06-07 13:44:59 +0200

    Initial commit of the psp-host-namespaces policy

---
+++ src/lib.rs
@@ -0,0 +1,258 @@
+extern crate wapc_guest as guest;
+use guest::prelude::*;
+
+use k8s_openapi::api::core::v1 as apicore;
+
+extern crate kubewarden_policy_sdk as kubewarden;
+use kubewarden::{protocol_version_guest, request::ValidationRequest, validate_settings};
+
+mod settings;
+use settings::Settings;
+
+#[no_mangle]
+pub extern "C" fn wapc_init() {
+    register_function("validate", validate);
+    register_function("validate_settings", validate_settings::<Settings>);
+    register_function("protocol_version", protocol_version_guest);
+}
+
+fn validate(payload: &[u8]) -> CallResult {
+    let validation_request: ValidationRequest<Settings> = ValidationRequest::new(payload)?;
+
+    let pod = match serde_json::from_value::<apicore::Pod>(validation_request.request.object) {
+        Ok(pod) => pod,
+        Err(_) => return kubewarden::accept_request(),
+    };
+
+    let pod_spec = pod.spec.ok_or("invalid pod spec")?;
+    let settings = validation_request.settings;
+
+    if pod_spec.host_ipc.unwrap_or(false) && !settings.allow_host_ipc {
+        return kubewarden::reject_request(
+            Some(format!(
+                "Pod {:?} has IPC enabled, but this is not allowed",
+                pod.metadata
+            )),
+            None,
+        );
+    }
+
+    if pod_spec.host_network.unwrap_or(false) && !settings.allow_host_network {
+        return kubewarden::reject_request(
+            Some(format!(
+                "Pod {:?} has host network enabled, but this is not allowed",
+                pod.metadata
+            )),
+            None,
+        );
+    }
+
+    if pod_spec.host_pid.unwrap_or(false) && !settings.allow_host_pid {
+        return kubewarden::reject_request(
+            Some(format!(
+                "Pod {:?} has host PID enabled, but this is not allowed",
+                pod.metadata
+            )),
+            None,
+        );
+    }
+
+    if !all_containers_allowed(
+        &pod_spec.init_containers.unwrap_or_default(),
+        &settings.allow_host_ports,
+    ) {
+        return kubewarden::reject_request(
+            Some(format!(
+                "Pod {:?} is using unallowed host ports in init containers",
+                pod.metadata
+            )),
+            None,
+        );
+    }
+
+    if !all_containers_allowed(&pod_spec.containers, &settings.allow_host_ports) {
+        return kubewarden::reject_request(
+            Some(format!(
+                "Pod {:?} is using unallowed host ports in containers",
+                pod.metadata
+            )),
+            None,
+        );
+    }
+
+    kubewarden::accept_request()
+}
+
+fn all_containers_allowed(
+    containers: &[apicore::Container],
+    allowed_host_ports: &[settings::PortRange],
+) -> bool {
+    containers.iter().all(|container| {
+        container
+            .ports
+            .clone()
+            .unwrap_or_default()
+            .iter()
+            .all(|port| match port.host_port {
+                Some(host_port) => allowed_host_ports
+                    .iter()
+                    .any(|allowed_host_port| allowed_host_port.in_range(host_port)),
+                None => true,
+            })
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use anyhow::Result;
+
+    use kubewarden_policy_sdk::test::Testcase;
+
+    #[test]
+    fn pod_host_ipc() -> Result<()> {
+        let request_file = "test_data/pod_host_ipc_enabled.json";
+
+        let tests = vec![
+            Testcase {
+                name: String::from("Host IPC namespace disallowed"),
+                fixture_file: String::from(request_file),
+                settings: Settings {
+                    allow_host_ipc: false,
+                    ..Default::default()
+                },
+                expected_validation_result: false,
+            },
+            Testcase {
+                name: String::from("Host IPC namespace allowed"),
+                fixture_file: String::from(request_file),
+                settings: Settings {
+                    allow_host_ipc: true,
+                    ..Default::default()
+                },
+                expected_validation_result: true,
+            },
+        ];
+
+        for tc in tests.iter() {
+            tc.eval(validate)?;
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn pod_host_network() -> Result<()> {
+        let request_file = "test_data/pod_host_network_enabled.json";
+
+        let tests = vec![
+            Testcase {
+                name: String::from("Host network namespace disallowed"),
+                fixture_file: String::from(request_file),
+                settings: Settings {
+                    allow_host_network: false,
+                    ..Default::default()
+                },
+                expected_validation_result: false,
+            },
+            Testcase {
+                name: String::from("Host network namespace allowed"),
+                fixture_file: String::from(request_file),
+                settings: Settings {
+                    allow_host_network: true,
+                    ..Default::default()
+                },
+                expected_validation_result: true,
+            },
+        ];
+
+        for tc in tests.iter() {
+            tc.eval(validate)?;
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn pod_host_pid() -> Result<()> {
+        let request_file = "test_data/pod_host_pid_enabled.json";
+
+        let tests = vec![
+            Testcase {
+                name: String::from("Host PID namespace disallowed"),
+                fixture_file: String::from(request_file),
+                settings: Settings {
+                    allow_host_pid: false,
+                    ..Default::default()
+                },
+                expected_validation_result: false,
+            },
+            Testcase {
+                name: String::from("Host PID namespace allowed"),
+                fixture_file: String::from(request_file),
+                settings: Settings {
+                    allow_host_pid: true,
+                    ..Default::default()
+                },
+                expected_validation_result: true,
+            },
+        ];
+
+        for tc in tests.iter() {
+            tc.eval(validate)?;
+        }
+
+        Ok(())
+    }
+
+    #[test]
+    fn pod_host_port() -> Result<()> {
+        let request_file = "test_data/pod_host_ports_443.json";
+
+        let tests = vec![
+            Testcase {
+                name: String::from("Host port 443 allowed"),
+                fixture_file: String::from(request_file),
+                settings: Settings {
+                    allow_host_ports: vec![
+                        settings::PortRange { min: 200, max: 300 },
+                        settings::PortRange { min: 443, max: 443 },
+                        settings::PortRange {
+                            min: 8000,
+                            max: 9000,
+                        },
+                    ],
+                    ..Default::default()
+                },
+                expected_validation_result: true,
+            },
+            Testcase {
+                name: String::from("Host port 443 allowed"),
+                fixture_file: String::from(request_file),
+                settings: Settings {
+                    allow_host_ports: vec![settings::PortRange { min: 80, max: 443 }],
+                    ..Default::default()
+                },
+                expected_validation_result: true,
+            },
+            Testcase {
+                name: String::from("Host port 443 disallowed"),
+                fixture_file: String::from(request_file),
+                settings: Settings {
+                    allow_host_ports: vec![settings::PortRange {
+                        min: 8000,
+                        max: 9000,
+                    }],
+                    ..Default::default()
+                },
+                expected_validation_result: false,
+            },
+        ];
+
+        for tc in tests.iter() {
+            tc.eval(validate)?;
+        }
+
+        Ok(())
+    }
+}
---
+++ src/settings.rs
@@ -0,0 +1,73 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub(crate) struct PortRange {
+    pub min: i32,
+    pub max: i32,
+}
+
+impl kubewarden::settings::Validatable for PortRange {
+    fn validate(&self) -> Result<(), String> {
+        if self.min > self.max {
+            return Err(format!(
+                "port min {} cannot be greater than max {}",
+                self.min, self.max
+            ));
+        }
+        Ok(())
+    }
+}
+
+impl PortRange {
+    pub fn in_range(&self, port: i32) -> bool {
+        port >= self.min && port <= self.max
+    }
+}
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub(crate) struct Settings {
+    pub allow_host_ipc: bool,
+    pub allow_host_network: bool,
+    pub allow_host_pid: bool,
+    pub allow_host_ports: Vec<PortRange>,
+}
+
+impl kubewarden::settings::Validatable for Settings {
+    fn validate(&self) -> Result<(), String> {
+        for port_range in self.allow_host_ports.iter() {
+            port_range.validate()?;
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use kubewarden_policy_sdk::settings::Validatable;
+
+    #[test]
+    fn validate_settings() {
+        let invalid_settings = Settings {
+            allow_host_ports: vec![PortRange { min: 443, max: 80 }],
+            ..Default::default()
+        };
+        assert!(!invalid_settings.validate().is_ok());
+
+        let valid_settings = Settings {
+            allow_host_ports: vec![PortRange { min: 80, max: 443 }],
+            ..Default::default()
+        };
+        assert!(valid_settings.validate().is_ok());
+
+        let valid_settings = Settings {
+            allow_host_ports: vec![
+                PortRange { min: 80, max: 80 },
+                PortRange { min: 443, max: 443 },
+            ],
+            ..Default::default()
+        };
+        assert!(valid_settings.validate().is_ok());
+    }
+}
